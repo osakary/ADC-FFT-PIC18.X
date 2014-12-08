baa9df5b3f0861895cf2330d9556f8e4938ddcf0
osakary <osakary@gmail.com>
2014-12-08 11:05:32 -0800

    working enough to demo

--- build/default/debug/fft.p1.d
+++
@@ -1,4 +0,0 @@
- build/default/debug/fft.d  \
- build/default/debug/fft.p1:  \
- fft.c  \
-fft.h 
--- build/default/debug/lcd3310_SPI.p1.d
+++
@@ -1,4 +0,0 @@
- build/default/debug/lcd3310_SPI.d  \
- build/default/debug/lcd3310_SPI.p1:  \
- lcd3310_SPI.c  \
-lcd3310.h 
